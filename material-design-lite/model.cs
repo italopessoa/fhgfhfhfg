bac3f25ba0be9c9e565d6c5983b1f4f9a2e08e8b
italopessoa <italo.pessoa@hotmail.com>
2017-04-13 10:14:53 -0300

    melhoria na definicao das classes.

--- material-design-lite/model.cs
+++ material-design-lite/model.cs
@@ -3,45 +3,52 @@ http://bitoftech.net/2015/03/11/asp-net-identity-2-1-roles-based-authorization-a
     criar regras no sistema, aluno, professor e coordenador
     utiliza essas regras para exibir apenas as pesquisas que
     o usuario podera responder
-* *//
+* */
 public class User
 {
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
-    public int GroupID { get; set; }
+    // public int RoleI { get; set; }
 }
 
 public class Survey
 {
     public int Id { get; set; }
+    public string Title { get; set; }
     public string Description { get; set; }
     public Datetime Start { get; set; }
     public Datetime End { get; set; }
 
-    public List<Question> Questions { get; set; }
+    // public List<Question> Questions { get; set; }
 }
 
 public class Subject
 {
     public int Id { get; set; }
+    public string Code { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
 }
 
 public class SubjectUser
 {
-    public int Id { get; set; }
+    //usar chave composta
+    // public int Id { get; set; }
     public int UserId { get; set; }
     public int SubjectId { get; set; }
-    public int SurveyId { get; set; }
+    // public int SurveyId { get; set; }
 }
 
 public class Question
 {
     public int Id { get; set; }
     public string Text { get; set; }
+    //adicionar regra pra saber quem pode responder a pergunta
+    public int TargetId { get; set; }
+    public bool Mandatory { get; set; }
+    public byte Type { get; set; }
 }
 
 public class SurveyQuestion
@@ -49,4 +56,14 @@ public class SurveyQuestion
     public int Id { get; set; }
     public int SurveyId { get; set; }
     public int QuestionId { get; set; } 
+}
+
+public class Answer
+{
+    public int Id { get; set; }
+    public int SurveyId { get; set; }
+    public int QuestionId { get; set; }
+    public int SubjectId { get; set; }
+    public int RoleId { get; set; }
+    public string Value { get; set; }
 }
