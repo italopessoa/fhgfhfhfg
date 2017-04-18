834a9f9026d149db2556635819c65884904aeaf1
Italo Pessoa <italo.pessoa@hotmail.com>
2017-04-17 22:22:23 -0300

    adicionado projeto android

---
+++ MaratonaXamarinIntermediario/MaratonaXamarin.AndroidApp/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace MaratonaXamarin.AndroidApp
+{
+    [Activity(Label = "MaratonaXamarin.AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            var button = this.FindViewById<Button>(Resource.Id.btnCarregar);
+            var listView = this.FindViewById<ListView>(Resource.Id.lvwItens);
+
+            button.Click += (sender, e) =>
+            {
+
+            };
+        }
+    }
+}
+
