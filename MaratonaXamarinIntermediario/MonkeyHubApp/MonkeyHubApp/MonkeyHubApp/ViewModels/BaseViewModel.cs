d2ff57e04e18bb7b21a63d53d02359c2208d22e4
Italo Pessoa <italo.pessoa@hotmail.com>
2017-04-25 19:32:25 -0300

    MOnkeyHubApp

---
+++ MaratonaXamarinIntermediario/MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyHubApp.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ MaratonaXamarinIntermediario/MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyHubApp.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _searchTerm;
+
+        public string SearchTerm
+        {
+            get { return _searchTerm; }
+            set { SetProperty<string>(ref _searchTerm, value); }
+        }
+
+    }
+}
