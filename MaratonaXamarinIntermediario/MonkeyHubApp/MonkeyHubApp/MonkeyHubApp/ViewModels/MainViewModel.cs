ee3898d77914aee7a87e297fb38983a1b391ccae
Italo Pessoa <italo.pessoa@hotmail.com>
2017-04-25 20:23:01 -0300

    adicionado command e alert

--- MaratonaXamarinIntermediario/MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MaratonaXamarinIntermediario/MonkeyHubApp/MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace MonkeyHubApp.ViewModels
 {
@@ -13,8 +14,34 @@ namespace MonkeyHubApp.ViewModels
         public string SearchTerm
         {
             get { return _searchTerm; }
-            set { SetProperty<string>(ref _searchTerm, value); }
+            set
+            {
+                if (SetProperty<string>(ref _searchTerm, value))
+                    SearchCommand.ChangeCanExecute();
+            }
         }
 
+
+        public Command SearchCommand { get; }
+
+        public MainViewModel()
+        {
+            SearchCommand = new Command(ExcecuteSearchCommand, CanExcecuteSearchCommand);
+        }
+
+        private async void ExcecuteSearchCommand(object obj)
+        {
+            await Task.Delay(1000);
+            bool r = await App.Current.MainPage.DisplayAlert("MonkeyHubApp", $"Você pesquisou por {SearchTerm}?", "Sim", "Não");
+            if (r)
+                await App.Current.MainPage.DisplayAlert("MonkeyHubApp", "Obrigado","Ok");
+            else
+                await App.Current.MainPage.DisplayAlert("MonkeyHubApp", "De nada", "Ok");
+        }
+
+        private bool CanExcecuteSearchCommand(object arg)
+        {
+            return !string.IsNullOrWhiteSpace(SearchTerm);
+        }
     }
 }
