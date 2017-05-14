0605d08242e019691ec9b24291ccd19067c855cf
Italo Pessoa <italo.pessoa@hotmail.com>
2017-05-14 19:05:19 -0300

    adicionada classe de servico gcm, adicionadas as bibliotecas azure client

---
+++ MaratonaXamarinIntermediario/MonkeyHubApp/MonkeyHubApp/MonkeyHubApp.Android/Services/GcmService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Gcm.Client;
+
+[assembly: Permission(Name = "@PACKAGE_NAME@.permission.C2D_MESSAGE")]
+[assembly: UsesPermission(Name = "@PACKAGE_NAME@.permission.C2D_MESSAGE")]
+[assembly: UsesPermission(Name = "com.google.android.c2dm.permission.RECEIVE")]
+[assembly: UsesPermission(Name = "android.permission.INTERNET")]
+[assembly: UsesPermission(Name = "android.permission.WAKE_LOCK")]
+//GET_ACCOUNTS is only needed for android versions 4.0.3 and below
+[assembly: UsesPermission(Name = "android.permission.GET_ACCOUNTS")]
+namespace MonkeyHubApp.Droid.Services
+{
+    [BroadcastReceiver(Permission = Gcm.Client.Constants.PERMISSION_GCM_INTENTS)]
+    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_MESSAGE }, Categories = new string[] { "@PACKAGE_NAME@" })]
+    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_REGISTRATION_CALLBACK }, Categories = new string[] { "@PACKAGE_NAME@" })]
+    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_LIBRARY_RETRY }, Categories = new string[] { "@PACKAGE_NAME@" })]
+    public class PushHandlerBroadcastReceiver : GcmBroadcastReceiverBase<GcmService>
+    {
+        private static string[] sENDER_IDS = new string[] { "44283727310" };
+
+        public static string[] SENDER_IDS { get => sENDER_IDS; }
+    }
+
+    public class GcmService : GcmServiceBase
+    {
+        MobileServiceCLient
+        protected override void OnError(Context context, string errorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnMessage(Context context, Intent intent)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnRegistered(Context context, string registrationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnUnRegistered(Context context, string registrationId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
